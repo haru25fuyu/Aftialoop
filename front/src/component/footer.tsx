f7948372b3f82560ba86f4fc63a3250eb671bebc
haru25fuyu <haru25fuyu@gmail.com>
2025-02-11 16:55:05 +0900

    フッターコンポーネントのスタイルを更新し、ナビゲーションを改善。新しいカートアイコンを追加し、レスポンシブデザインを強化。

--- front/src/component/footer.tsx
+++ front/src/component/footer.tsx
@@ -7,34 +7,24 @@ import '../css/Footer.css';
 export const Footer: React.FC = () => {
 
     return (
-        <footer>
-            <div className="category">
-                <ul>
-                    <li><Link to="/">TOP</Link></li>
-                    <li><Link to="/payment">商品一覧</Link></li>
-                    <li><Link to="/mypage">マイページ</Link></li>
-                    <li><Link to="/mypage">お問い合わせ</Link></li>
-                    <li><Link to="/mypage">お問い合わせ</Link></li>
-                </ul>
+        <footer className="flex items-center justify-between w-full bg-white py-4 px-6">
+            {/* ロゴ */}
+            <div className="w-24 h-24 flex-shrink-0">
+                <img src="/data/Logo.JPG" alt="ロゴ" className="w-full h-full object-contain" />
             </div>
-            <div className="footer">
-                <img className='fotter-logo' src="/../data/Logo.JPG"></img>
-                <div className="fotter-list">
-                    <ul>
-                        <li><Link to="/">TOP</Link></li>
-                        <li><Link to="/payment">商品一覧</Link></li>
-                        <li><Link to="/mypage">マイページ</Link></li>
-                        <li><Link to="/mypage">お問い合わせ</Link></li>
-                    </ul>
-                    <br />
-                    <ul>
-                        <li><Link to="/">TOP</Link></li>
-                        <li><Link to="/payment">商品一覧</Link></li>
-                        <li><Link to="/mypage">マイページ</Link></li>
-                        <li><Link to="/mypage">お問い合わせ</Link></li>
-                    </ul>
-                </div>
 
+            {/* ナビゲーション */}
+            <nav className="flex space-x-6 text-gray-700 text-lg">
+                <Link to="/" className="hover:text-black">TOP</Link>
+                <Link to="/payment" className="hover:text-black">商品一覧</Link>
+                <Link to="/mypage" className="hover:text-black">マイページ</Link>
+                <Link to="/contact" className="hover:text-black">お問い合わせ</Link>
+            </nav>
+
+            {/* SNS & カート */}
+            <div className="flex items-center space-x-4">
+                <img src="/data/Instagram_Glyph_Black.png" alt="Instagram" className="w-8 h-8" />
+                <img src="/data/cart.png" alt="カート" className="w-8 h-8" />
             </div>
         </footer>
     );
